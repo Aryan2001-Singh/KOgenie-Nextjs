26a4788da4d1e8c6a79aaf785a8a17f0642448a3
Aryan Singh <aryan.aryansingh.singh008@gmail.com>
2024-10-22 14:24:49 +0530

    changes

--- lib/db.ts
+++ lib/db.ts
@@ -2,14 +2,12 @@ import { PrismaClient } from "@prisma/client";
 
 // Extend the globalThis interface to include the prisma property
 declare global {
-  // No need for namespaces, directly extend globalThis
-  interface Global {
-    prisma: PrismaClient | undefined;
-  }
-
-  var prisma: PrismaClient | undefined; // Use let or const as needed
+  // Declare prisma as a constant in globalThis, but it should not be reassigned.
+  var prisma: PrismaClient | undefined;
 }
 
 export const db = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = db;
+}
